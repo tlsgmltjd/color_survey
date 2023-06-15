6f34f0d988a6ac9a7a9ec7ea34e933e23bf8eff3
신희성 <vvwv3092@gmail.com>
2023-06-15 22:55:42 +0900

    Update SelectBox

--- src/PageContainer/SelectPage/index.js
+++ src/PageContainer/SelectPage/index.js
@@ -1,11 +1,79 @@
 import * as S from "./style";
 import * as C from "../../components";
+import { useState } from "react";
 
 const SelectPage = () => {
+  const optionGroups = [
+    [
+      { value: "E", label: "외향형" },
+      { value: "I", label: "내향형" },
+    ],
+    [
+      { value: "S", label: "감각형" },
+      { value: "N", label: "직관형" },
+    ],
+    [
+      { value: "T", label: "사고형" },
+      { value: "F", label: "감정형" },
+    ],
+    [
+      { value: "J", label: "판단형" },
+      { value: "P", label: "인식형" },
+    ],
+  ];
+
+  const [mbti, setMbti] = useState(["E", "S", "T", "J"]);
+
+  const updatedMbti = (index, pos, item) => {
+    const updatedMbti = [...mbti];
+    updatedMbti[index] = item[pos].value;
+    setMbti(updatedMbti);
+  };
+
   return (
     <S.SelectContainer>
       <C.MbtiSelectBox />
-      <S.SelectSection>MBTI</S.SelectSection>
+
+      <S.SelectSection>
+        <S.SectionTitle>MBTI</S.SectionTitle>
+      </S.SelectSection>
+
+      <S.SelectBtnContainer>
+        {optionGroups.map((item, index) => (
+          <S.SelectBtnBox>
+            <S.SelectBtn
+              selected={mbti[index]}
+              currentMbti={item[0].value}
+              onClick={() => {
+                updatedMbti(index, 0, item);
+              }}
+            >
+              <S.SelectBtnBoldText
+                selected={mbti[index]}
+                currentMbti={item[0].value}
+              >
+                {item[0].value}
+              </S.SelectBtnBoldText>
+              {item[0].label}
+            </S.SelectBtn>
+            <S.SelectBtn
+              selected={mbti[index]}
+              currentMbti={item[1].value}
+              onClick={() => {
+                updatedMbti(index, 1, item);
+              }}
+            >
+              <S.SelectBtnBoldText
+                selected={mbti[index]}
+                currentMbti={item[1].value}
+              >
+                {item[1].value}
+              </S.SelectBtnBoldText>
+              {item[1].label}
+            </S.SelectBtn>
+          </S.SelectBtnBox>
+        ))}
+      </S.SelectBtnContainer>
     </S.SelectContainer>
   );
 };
--- src/PageContainer/SelectPage/style.js
+++ src/PageContainer/SelectPage/style.js
@@ -8,6 +8,52 @@ export const SelectContainer = styled.div`
 export const SelectSection = styled.header`
   width: 100%;
   display: flex;
+
   justify-content: space-between;
   align-items: center;
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 25px;
+  font-weight: 500;
+  color: #464e5e;
+`;
+
+export const SelectBtnContainer = styled.div``;
+
+export const SelectBtnBox = styled.div`
+  display: flex;
+  margin: 15px 0;
+  gap: 15px;
+`;
+
+export const SelectBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-grow: 1;
+  height: 80px;
+  font-size: 25px;
+  color: #8892a6;
+  padding: 0 40px;
+
+  background: ${(props) =>
+    props.selected == props.currentMbti ? "#464E5E" : "white"};
+
+  border: 2px solid #e7ebf2;
+  border-radius: 13px;
+
+  cursor: pointer;
+
+  &:hover {
+    background: ${(props) =>
+      props.selected == props.currentMbti ? "#464E5E" : "#f5f7fb"};
+  }
+`;
+
+export const SelectBtnBoldText = styled.span`
+  font-size: 35px;
+  font-weight: 600;
+  color: ${(props) =>
+    props.selected == props.currentMbti ? "white" : "#464e5e"};
 `;
