b05cde127ec0f157c64bda24dd221e53d3551bd6
신희성 <vvwv3092@gmail.com>
2023-06-17 16:39:12 +0900

    sortedData comment

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
   // 더 빨리 할 일 : FilterBox의 X를 눌렀을 때 data들이 다시 돌아오게 해야함
   // -> selectdFilter 값이 없어졌을 때, 데이터를 다시 불러와서 data state에 저장..?
 
+  // sortedData 라는 state를 만들어서 filter가 선택되면 sortedData state에
+  // filtered 된 데이터 저장, 그리고 List로 출력될 때 filter가 선택되었다면
+  // sortedData를 출력시키게 함
+
   // 할일 : 전역상태관리하기 - props 드릴링이 시작되었다.. data, selectdFilter state 관리하기
 
   useEffect(() => {
