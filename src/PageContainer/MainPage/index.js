09d316bbccb74ef440fe390b8df39556fed16d55
신희성 <vvwv3092@gmail.com>
2023-06-19 08:24:20 +0900

    Fixed Error

--- src/PageContainer/MainPage/index.js
+++ src/PageContainer/MainPage/index.js
@@ -11,6 +11,11 @@ const MainPage = ({ data, setData, selectdFilter, setSelectdFilter }) => {
     setSelectdFilter("");
   };
 
+  // 필터된 상태에서 컬러를 추가할 시 필터된 List에 추가된 컬러가 추가되는 오류 수정
+  const handleSubmit = () => {
+    setSelectdFilter("");
+  };
+
   return (
     <>
       <S.MainContainer>
@@ -28,7 +33,7 @@ const MainPage = ({ data, setData, selectdFilter, setSelectdFilter }) => {
         </S.MainHeader>
         <S.MainContent>
           <Link to="select">
-            <S.ColorSurveyBtn>
+            <S.ColorSurveyBtn onClick={handleSubmit}>
               <S.ColorSurveyBtnText>+ 새 컬러 등록하기</S.ColorSurveyBtnText>
             </S.ColorSurveyBtn>
           </Link>
